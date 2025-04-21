89ce0a1de24912d2876197c859fafa030447b375
mathos <>
2025-04-21 21:14:29 +0000

    Adds basic test [7348365717] Adds ruff formatting [f4bfe769ac]
    FossilOrigin-Name: ac4c110f21fee6b447744d2aac1c997938f8644145304a2f2d77175fac72d3fc

--- README.md
+++ README.md
@@ -8,3 +8,20 @@ Should support query generation for
 In two ways 
 1. Snowflake
 2. Naive
+
+# Main program
+For documentation you may run
+```bash
+pixi run main --help
+```
+# Testing
+To run the test you may use
+```bash
+pixi run test
+```
+
+# Fix-lint
+To lint the files you may use 
+```bash
+pixi run lint
+```
--- src/query_generator/main.py
+++ src/query_generator/main.py
@@ -1,15 +1,18 @@
 import typer
-from rich import print 
+from rich import print
 
-app = typer.Typer(name = 'Query Generation')
+app = typer.Typer(name="Query Generation")
+
 
 @app.command()
 def drive():
     print("[green] Some green text[/green]")
 
+
 @app.command()
 def stop():
     print("[red] Some red text[/red]")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app()
