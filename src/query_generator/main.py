6f471a8bf81ec182b0dd2bedd7d7b1bd4ab784fe
mathos <>
2025-04-24 20:58:22 +0000

    makes mypy strict to force me to type stuff
    FossilOrigin-Name: a8463d4e85953db33371fa5cb2125e0423c1d7b0f39ab879d2709f09d02a654e

--- src/query_generator/main.py
+++ src/query_generator/main.py
@@ -4,13 +4,18 @@ from rich import print
 app = typer.Typer(name="Query Generation")
 
 
-@app.command()
-def drive():
-    print("[green] Some green text[/green]")
+def square(x: int) -> int:
+    return x**2
 
 
 @app.command()
-def stop():
+def drive() -> None:
+    a = 2
+    print(f"[green] Some green {square(a)} text[/green]")
+
+
+@app.command()
+def stop() -> None:
     print("[red] Some red text[/red]")
 
 
