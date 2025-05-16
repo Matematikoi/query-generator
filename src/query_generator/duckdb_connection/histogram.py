26d60214ecc497ad515721a4a99d0b200f3dd516
mathos <>
2025-05-16 08:44:27 +0000

    removes unnecessary file
    FossilOrigin-Name: 35470261baac0711e26fc59167e68d3bcb7f97140f15133b49416575d467f1e7

--- src/query_generator/duckdb_connection/histogram.py
+++
@@ -1,8 +0,0 @@
-class DuckDBHistogram:
-  def __init__(self, bins: list[str], count: list[int]) -> None:
-    self.bins = bins
-    self.count = count
-    self.get_lower_upper_bounds()
-
-  def get_lower_upper_bounds(self) -> None:
-    pass
