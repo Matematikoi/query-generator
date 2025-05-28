67beea0edd96f319ecc1e4a726873e997958e3ee
mathos <>
2025-05-28 12:39:39 +0000

    Adds JOB support
    FossilOrigin-Name: 3ab34236e13402750b79eab4880d2499fff50f4b6f349f3eab8dc1b3bf7b2e3a

--- src/query_generator/database_schemas/job.py
+++ src/query_generator/database_schemas/job.py
@@ -1,10 +1,15 @@
 from typing import Any
 
+from query_generator.utils.exceptions import (
+  InvalidForeignKeyError,
+  TableNotFoundError,
+)
 
-def get_tpcds_table_info() -> Any:
+
+def get_job_table_info() -> tuple[dict[str, dict[str, Any]], list[str]]:
   tables = {
     "aka_name": {
-      "alias" : "an",
+      "alias": "an",
       "columns": {
         "id": {"min": "placeholder", "max": "placeholder"},
         "person_id": {"min": "placeholder", "max": "placeholder"},
@@ -14,10 +19,17 @@ def get_tpcds_table_info() -> Any:
         "name_pcode_nf": {"min": "placeholder", "max": "placeholder"},
         "surname_pcode": {"min": "placeholder", "max": "placeholder"},
         "md5sum": {"min": "placeholder", "max": "placeholder"},
-      }
+      },
+      "foreign_keys": [
+        {
+          "column": "person_id",
+          "ref_table": "name",
+          "ref_column": "id",
+        },
+      ],
     },
     "aka_title": {
-      "alias" : "at",
+      "alias": "at",
       "columns": {
         "id": {"min": "placeholder", "max": "placeholder"},
         "movie_id": {"min": "placeholder", "max": "placeholder"},
@@ -31,10 +43,11 @@ def get_tpcds_table_info() -> Any:
         "episode_nr": {"min": "placeholder", "max": "placeholder"},
         "note": {"min": "placeholder", "max": "placeholder"},
         "md5sum": {"min": "placeholder", "max": "placeholder"},
-      }
+      },
+      "foreign_keys": [],
     },
     "cast_info": {
-      "alias" : "ci",
+      "alias": "ci",
       "columns": {
         "id": {"min": "placeholder", "max": "placeholder"},
         "person_id": {"min": "placeholder", "max": "placeholder"},
@@ -43,10 +56,27 @@ def get_tpcds_table_info() -> Any:
         "note": {"min": "placeholder", "max": "placeholder"},
         "nr_order": {"min": "placeholder", "max": "placeholder"},
         "role_id": {"min": "placeholder", "max": "placeholder"},
-      }
+      },
+      "foreign_keys": [
+        {
+          "column": "person_id",
+          "ref_table": "aka_name",
+          "ref_column": "person_id",
+        },
+        {
+          "column": "person_role_id",
+          "ref_table": "char_name",
+          "ref_column": "id",
+        },
+        {
+          "column": "role_id",
+          "ref_table": "role_type",
+          "ref_column": "id",
+        },
+      ],
     },
     "char_name": {
-      "alias" : "chn",
+      "alias": "chn",
       "columns": {
         "id": {"min": "placeholder", "max": "placeholder"},
         "name": {"min": "placeholder", "max": "placeholder"},
@@ -55,17 +85,19 @@ def get_tpcds_table_info() -> Any:
         "name_pcode_nf": {"min": "placeholder", "max": "placeholder"},
         "surname_pcode": {"min": "placeholder", "max": "placeholder"},
         "md5sum": {"min": "placeholder", "max": "placeholder"},
-      }
+      },
+      "foreign_keys": [],
     },
     "comp_cast_type": {
-      "alias" : "cct",
+      "alias": "cct",
       "columns": {
         "id": {"min": "placeholder", "max": "placeholder"},
         "kind": {"min": "placeholder", "max": "placeholder"},
-      }
+      },
+      "foreign_keys": [],
     },
     "company_name": {
-      "alias" : "cn",
+      "alias": "cn",
       "columns": {
         "id": {"min": "placeholder", "max": "placeholder"},
         "name": {"min": "placeholder", "max": "placeholder"},
@@ -74,102 +106,160 @@ def get_tpcds_table_info() -> Any:
         "name_pcode_nf": {"min": "placeholder", "max": "placeholder"},
         "name_pcode_sf": {"min": "placeholder", "max": "placeholder"},
         "md5sum": {"min": "placeholder", "max": "placeholder"},
-      }
+      },
+      "foreign_keys": [],
     },
     "company_type": {
-      "alias" : "ct",
+      "alias": "ct",
       "columns": {
         "id": {"min": "placeholder", "max": "placeholder"},
         "kind": {"min": "placeholder", "max": "placeholder"},
-      }
+      },
+      "foreign_keys": [],
     },
     "complete_cast": {
-      "alias" : "cc",
+      "alias": "cc",
       "columns": {
         "id": {"min": "placeholder", "max": "placeholder"},
         "movie_id": {"min": "placeholder", "max": "placeholder"},
         "subject_id": {"min": "placeholder", "max": "placeholder"},
         "status_id": {"min": "placeholder", "max": "placeholder"},
-      }
+      },
+      "foreign_keys": [
+        {
+          "column": "subject_id",
+          "ref_table": "comp_cast_type",
+          "ref_column": "id",
+        },
+        {
+          "column": "status_id",
+          "ref_table": "comp_cast_type",
+          "ref_column": "id",
+        },
+      ],
     },
     "info_type": {
-      "alias" : "it",
+      "alias": "it",
       "columns": {
         "id": {"min": "placeholder", "max": "placeholder"},
         "info": {"min": "placeholder", "max": "placeholder"},
-      }
+      },
+      "foreign_keys": [],
     },
     "keyword": {
-      "alias" : "k",
+      "alias": "k",
       "columns": {
         "id": {"min": "placeholder", "max": "placeholder"},
         "keyword": {"min": "placeholder", "max": "placeholder"},
         "phonetic_code": {"min": "placeholder", "max": "placeholder"},
-      }
+      },
+      "foreign_keys": [],
     },
     "kind_type": {
-      "alias" : "kt",
+      "alias": "kt",
       "columns": {
         "id": {"min": "placeholder", "max": "placeholder"},
         "kind": {"min": "placeholder", "max": "placeholder"},
-      }
+      },
+      "foreign_keys": [],
     },
     "link_type": {
-      "alias" : "lt",
+      "alias": "lt",
       "columns": {
         "id": {"min": "placeholder", "max": "placeholder"},
         "link": {"min": "placeholder", "max": "placeholder"},
-      }
+      },
+      "foreign_keys": [],
     },
     "movie_companies": {
-      "alias" : "mc",
+      "alias": "mc",
       "columns": {
         "id": {"min": "placeholder", "max": "placeholder"},
         "movie_id": {"min": "placeholder", "max": "placeholder"},
         "company_id": {"min": "placeholder", "max": "placeholder"},
         "company_type_id": {"min": "placeholder", "max": "placeholder"},
         "note": {"min": "placeholder", "max": "placeholder"},
-      }
+      },
+      "foreign_keys": [
+        {
+          "column": "company_id",
+          "ref_table": "company_name",
+          "ref_column": "id",
+        },
+        {
+          "column": "company_type_id",
+          "ref_table": "company_type",
+          "ref_column": "id",
+        },
+      ],
     },
     "movie_info": {
-      "alias" : "mi",
+      "alias": "mi",
       "columns": {
         "id": {"min": "placeholder", "max": "placeholder"},
         "movie_id": {"min": "placeholder", "max": "placeholder"},
         "info_type_id": {"min": "placeholder", "max": "placeholder"},
         "info": {"min": "placeholder", "max": "placeholder"},
         "note": {"min": "placeholder", "max": "placeholder"},
-      }
+      },
+      "foreign_keys": [
+        {
+          "column": "info_type_id",
+          "ref_table": "info_type",
+          "ref_column": "id",
+        },
+      ],
     },
     "movie_info_idx": {
-      "alias" : "mi_idx",
+      "alias": "mi_idx",
       "columns": {
         "id": {"min": "placeholder", "max": "placeholder"},
         "movie_id": {"min": "placeholder", "max": "placeholder"},
         "info_type_id": {"min": "placeholder", "max": "placeholder"},
         "info": {"min": "placeholder", "max": "placeholder"},
         "note": {"min": "placeholder", "max": "placeholder"},
-      }
+      },
+      "foreign_keys": [
+        {
+          "column": "info_type_id",
+          "ref_table": "info_type",
+          "ref_column": "id",
+        },
+      ],
     },
     "movie_keyword": {
-      "alias" : "mk",
+      "alias": "mk",
       "columns": {
         "id": {"min": "placeholder", "max": "placeholder"},
         "movie_id": {"min": "placeholder", "max": "placeholder"},
         "keyword_id": {"min": "placeholder", "max": "placeholder"},
-      }
+      },
+      "foreign_keys": [
+        {
+          "column": "keyword_id",
+          "ref_table": "keyword",
+          "ref_column": "id",
+        },
+      ],
     },
     "movie_link": {
-      "alias" : "ml",
+      "alias": "ml",
       "columns": {
         "id": {"min": "placeholder", "max": "placeholder"},
         "movie_id": {"min": "placeholder", "max": "placeholder"},
         "linked_movie_id": {"min": "placeholder", "max": "placeholder"},
         "link_type_id": {"min": "placeholder", "max": "placeholder"},
-      }
+      },
+      "foreign_keys": [
+        {
+          "column": "link_type_id",
+          "ref_table": "link_type",
+          "ref_column": "id",
+        },
+      ],
     },
     "name": {
-      "alias" : "n",
+      "alias": "n",
       "columns": {
         "id": {"min": "placeholder", "max": "placeholder"},
         "name": {"min": "placeholder", "max": "placeholder"},
@@ -180,27 +270,30 @@ def get_tpcds_table_info() -> Any:
         "name_pcode_nf": {"min": "placeholder", "max": "placeholder"},
         "surname_pcode": {"min": "placeholder", "max": "placeholder"},
         "md5sum": {"min": "placeholder", "max": "placeholder"},
-      }
+      },
+      "foreign_keys": [],
     },
     "person_info": {
-      "alias" : "pi",
+      "alias": "pi",
       "columns": {
         "id": {"min": "placeholder", "max": "placeholder"},
         "person_id": {"min": "placeholder", "max": "placeholder"},
         "info_type_id": {"min": "placeholder", "max": "placeholder"},
         "info": {"min": "placeholder", "max": "placeholder"},
         "note": {"min": "placeholder", "max": "placeholder"},
-      }
+      },
+      "foreign_keys": [],
     },
     "role_type": {
-      "alias" : "rt",
+      "alias": "rt",
       "columns": {
         "id": {"min": "placeholder", "max": "placeholder"},
         "role": {"min": "placeholder", "max": "placeholder"},
-      }
+      },
+      "foreign_keys": [],
     },
     "title": {
-      "alias" : "t",
+      "alias": "t",
       "columns": {
         "id": {"min": "placeholder", "max": "placeholder"},
         "title": {"min": "placeholder", "max": "placeholder"},
@@ -214,7 +307,73 @@ def get_tpcds_table_info() -> Any:
         "episode_nr": {"min": "placeholder", "max": "placeholder"},
         "series_years": {"min": "placeholder", "max": "placeholder"},
         "md5sum": {"min": "placeholder", "max": "placeholder"},
-      }
+      },
+      "foreign_keys": [
+        {
+          "column": "id",
+          "ref_table": "complete_cast",
+          "ref_column": "movie_id",
+        },
+        {
+          "column": "id",
+          "ref_table": "aka_title",
+          "ref_column": "movie_id",
+        },
+        {
+          "column": "id",
+          "ref_table": "movie_link",
+          "ref_column": "movie_id",
+        },
+        {
+          "column": "id",
+          "ref_table": "movie_link",
+          "ref_column": "linked_movie_id",
+        },
+        {
+          "column": "id",
+          "ref_table": "cast_info",
+          "ref_column": "movie_id",
+        },
+        {
+          "column": "kind_id",
+          "ref_table": "kind_type",
+          "ref_column": "id",
+        },
+        {
+          "column": "id",
+          "ref_table": "movie_info",
+          "ref_column": "movie_id",
+        },
+        {
+          "column": "id",
+          "ref_table": "movie_info_idx",
+          "ref_column": "movie_id",
+        },
+        {
+          "column": "id",
+          "ref_table": "movie_keyword",
+          "ref_column": "movie_id",
+        },
+        {
+          "column": "id",
+          "ref_table": "movie_companies",
+          "ref_column": "movie_id",
+        },
+      ],
     },
   }
-  return tables, False
+  for table_name, table_info in tables.items():
+    for fk in table_info["foreign_keys"]:
+      ref_table = fk["ref_table"]
+      if fk["column"] not in tables[table_name]["columns"]:
+        raise InvalidForeignKeyError(table_name, fk["column"])
+      if ref_table not in tables:
+        raise TableNotFoundError(fk["ref_table"])
+      if fk["ref_column"] not in tables[ref_table]["columns"]:
+        raise InvalidForeignKeyError(
+          ref_table,
+          fk["ref_column"],
+        )
+
+  fact_tables = ["title"]
+  return tables, fact_tables
