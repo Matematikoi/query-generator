96df10b0b84263028ebc8659a41434e13b945059
mathos <>
2025-09-02 18:18:45 +0000

    Adds small refactor to the union
    FossilOrigin-Name: d70840e5480bc310b3300057ab47d70dcc08f0e87182a9bccc14d4bb0aac7682

--- src/query_generator/tools/union_queries.py
+++ src/query_generator/tools/union_queries.py
@@ -2,21 +2,27 @@ import random
 import re
 from pathlib import Path
 
+import polars as pl
+
 from query_generator.utils.exceptions import InvalidQueryError
 from query_generator.utils.utils import set_seed
 
 MINIMUM_QUERIES_TO_UNION = 2
 
 
+def get_select_list(query: str) -> str:
+  match = re.search(r"SELECT (.*) FROM", query)
+  if match is None:
+    raise InvalidQueryError(query)
+  return match.group(1)
+
+
 def get_new_query(sampled_query_paths: list[Path]) -> str:
   """Generate a new query by combining sampled queries."""
   queries = [path.read_text() for path in sampled_query_paths]
-  match = re.search(r"SELECT (.*) FROM", queries[0])
-  if match is None:
-    raise InvalidQueryError(queries[0])
-  base_sql_statement = match.group(1)
+  base_select_list = get_select_list(queries[0])
   new_queries = [
-    re.sub(r"SELECT (.*) FROM", f"SELECT {base_sql_statement} FROM", query)
+    re.sub(r"SELECT (.*) FROM", f"SELECT {base_select_list} FROM", query)
     for query in queries
   ]
   return " UNION ALL ".join(new_queries)
@@ -26,24 +32,27 @@ def union_queries(
   parquet_path: Path, destination_path: Path, max_queries: int
 ) -> None:
   set_seed()
-  path_to_dirs = parquet_path.parent
+  df_input = pl.read_parquet(parquet_path)
   cnt = 0
-  for dir in path_to_dirs.iterdir():
-    if dir.is_dir():
-      query_files = list(dir.glob("*.sql"))
+  for _, df_signature in df_input.group_by(
+    "subgraph_signature", maintain_order=True
+  ):
+    queries_relative_paths = df_signature.get_column("relative_path").to_list()
+    queries_paths = [
+      Path(parquet_path.parent) / i for i in queries_relative_paths
+    ]
 
-      if len(query_files) < MINIMUM_QUERIES_TO_UNION:
-        continue
+    if len(queries_paths) < MINIMUM_QUERIES_TO_UNION:
+      continue
 
-      sampled_query_paths = random.sample(
-        query_files,
-        k=random.randint(
-          MINIMUM_QUERIES_TO_UNION, min(max_queries, len(query_files))
-        ),
-      )
-
-      new_query = get_new_query(sampled_query_paths)
-      new_query_path = destination_path / "union" / f"union-{cnt}.sql"
-      new_query_path.parent.mkdir(parents=True, exist_ok=True)
-      new_query_path.write_text(new_query)
-      cnt += 1
+    sampled_query_paths = random.sample(
+      queries_paths,
+      k=random.randint(
+        MINIMUM_QUERIES_TO_UNION, min(max_queries, len(queries_paths))
+      ),
+    )
+    new_query = get_new_query(sampled_query_paths)
+    new_query_path = destination_path / "union" / f"union-{cnt}.sql"
+    new_query_path.parent.mkdir(parents=True, exist_ok=True)
+    new_query_path.write_text(new_query)
+    cnt += 1
