2272cd069272ad28627d5f0599ef133e6bb5fdee
mathos <>
2025-09-08 21:37:26 +0000

    Refactors the name of the params_config
    FossilOrigin-Name: 6236cebc0250ac384ec8adddadf9035fb63c68fa9e0f884fb91dfce4356f066b

--- tests/command_line/test_example_tomls.py
+++ tests/command_line/test_example_tomls.py
@@ -10,38 +10,28 @@ from query_generator.utils.params import (
 base_path = Path(__file__).parent.parent.parent
 
 
-@pytest.mark.parametrize(
-  "file_name", ["tpcds_dev.toml", "tpcds.toml", "job.toml", "job_dev.toml"]
-)
-def test_param_search(file_name):
+def test_param_search():
   """
   Test that the example toml provided for the param-search
   endpoint are valid
   """
 
-  file_path = base_path / f"params_config/synthetic_queries/{file_name}"
-  params = read_and_parse_toml(
-    Path(file_path),
-    SyntheticQueriesEndpoint,
-  )
-  assert params is not None
+  file_path = base_path / f"params_config/synthetic_queries/"
+  for file in Path(file_path).glob("*.toml"):
+    file_path = base_path / f"params_config/synthetic_queries/{file.name}"
+    params = read_and_parse_toml(
+      Path(file_path),
+      SyntheticQueriesEndpoint,
+    )
+    assert params is not None
 
 
-@pytest.mark.parametrize(
-  "file_name",
-  [
-    "tpcds_dev.toml",
-    "tpcds.toml",
-    "tpcds_devstral.toml",
-    "tpcds_gemma.toml",
-    "tpcds_llama4.toml",
-    "tpcds_qwen.toml",
-  ],
-)
-def test_add_complex_queries(file_name):
-  file_path = base_path / f"params_config/complex_queries/{file_name}"
-  params = read_and_parse_toml(
-    Path(file_path),
-    ExtensionAndLLMEndpoint,
-  )
-  assert params is not None
+def test_add_complex_queries():
+  file_path = base_path / f"params_config/extensions_and_llm"
+  for file in Path(file_path).glob("*.toml"):
+    file_path = base_path / f"params_config/extensions_and_llm/{file.name}"
+    params = read_and_parse_toml(
+      Path(file_path),
+      ExtensionAndLLMEndpoint,
+    )
+    assert params is not None
