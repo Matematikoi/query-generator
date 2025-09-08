359959e3da65fb04d398cb185f38a75ca4e8c062
mathos <>
2025-09-08 21:45:03 +0000

    Adds more tests for the toml configs that are there
    FossilOrigin-Name: f98e55806fb5c3b766ba15ad2e7a7f84c4938629acb863cc5f4f2b1dcaf9f6bb

--- tests/command_line/test_example_tomls.py
+++ tests/command_line/test_example_tomls.py
@@ -3,6 +3,9 @@ import pytest
 
 from query_generator.utils.params import (
   ExtensionAndLLMEndpoint,
+  FilterEndpoint,
+  GenerateDBEndpoint,
+  HistogramEndpoint,
   SyntheticQueriesEndpoint,
   read_and_parse_toml,
 )
@@ -10,7 +13,7 @@ from query_generator.utils.params import (
 base_path = Path(__file__).parent.parent.parent
 
 
-def test_param_search():
+def test_synthetic_queries():
   """
   Test that the example toml provided for the param-search
   endpoint are valid
@@ -18,20 +21,48 @@ def test_param_search():
 
   file_path = base_path / f"params_config/synthetic_queries/"
   for file in Path(file_path).glob("*.toml"):
-    file_path = base_path / f"params_config/synthetic_queries/{file.name}"
     params = read_and_parse_toml(
-      Path(file_path),
+      file,
       SyntheticQueriesEndpoint,
     )
     assert params is not None
 
 
-def test_add_complex_queries():
+def test_extensions_and_llm():
   file_path = base_path / f"params_config/extensions_and_llm"
   for file in Path(file_path).glob("*.toml"):
-    file_path = base_path / f"params_config/extensions_and_llm/{file.name}"
     params = read_and_parse_toml(
-      Path(file_path),
+      file,
       ExtensionAndLLMEndpoint,
+    )
+    assert params is not None
+
+
+def test_generate_db_toml():
+  file_path = base_path / f"params_config/generate_db"
+  for file in Path(file_path).glob("*.toml"):
+    params = read_and_parse_toml(
+      file,
+      GenerateDBEndpoint,
+    )
+    assert params is not None
+
+
+def test_make_histograms():
+  file_path = base_path / f"params_config/make_histograms"
+  for file in Path(file_path).glob("*.toml"):
+    params = read_and_parse_toml(
+      file,
+      HistogramEndpoint,
+    )
+    assert params is not None
+
+
+def test_filter_synthetic():
+  file_path = base_path / f"params_config/filter_synthetic"
+  for file in Path(file_path).glob("*.toml"):
+    params = read_and_parse_toml(
+      file,
+      FilterEndpoint,
     )
     assert params is not None
