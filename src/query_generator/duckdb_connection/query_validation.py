72299e6e16e97ef97b0188b72dd058a582779632
Gabriel Lozano <matematikoi@222-161.wifi-inria-saclay.saclay.inria.fr>
2025-11-25 17:06:10 +0100

    adds duckdb query validator

---
+++ src/query_generator/duckdb_connection/query_validation.py
@@ -0,0 +1,44 @@
+import logging
+
+import duckdb
+
+from query_generator.duckdb_connection.utils import get_tables
+
+logger = logging.getLogger(__name__)
+
+
+class DuckDBQueryValidator:
+  def __init__(self, database_path: str) -> None:
+    self.database_path = database_path
+    self.connect_to_database()
+    self.get_tables()
+
+  def get_tables(self) -> None:
+    self.tables = get_tables(self.conn)
+
+  def test_and_fix_connection(self) -> None:
+    """Test connection and reconnects if any problem arise."""
+    try:
+      # Simple query to verify the connection responds correctly
+      self.conn.execute(f"SELECT (*) FROM {self.tables[0]};").fetchall()
+    except Exception as e:
+      logger.exception("Error in basic SQL query, restarting connection")
+      logger.debug(f"Error produced: {e}")
+      self.conn.close()
+      self.connect_to_database()
+    else:
+      logger.debug("Database tested, no connection problem found.")
+
+  def is_query_valid(self, query: str) -> tuple[bool, Exception]:
+    self.test_and_fix_connection()
+    try:
+      self.conn.sql(query).fetchone()
+    except Exception as e:
+      logger.exception("DuckDB failed to run the provided query.")
+      logger.debug(f"Query that fail to run: \n```sql\n{query}\n```")
+      return False, e
+    else:
+      return True, Exception("No exception found while running the query")
+
+  def connect_to_database(self) -> None:
+    self.conn = duckdb.connect(database=self.database_path, read_only=True)
